49bc1fd5bc99575c621fecb7997c45483743e09b
TimurKiyivinski <deathseclipse@gmail.com>
2014-11-26 11:31:55 +0800

    Iteration 2.1

--- Dice.cpp
+++ Dice.cpp
@@ -8,29 +8,36 @@
 
 using namespace std;
 
+// Empty constructor as nothing needs to be initialized
 Dice::Dice() {}
 
 Dice::~Dice()
 {
+    // Loops through all dies and frees the memory
     for (Die *d: _die)
         delete d;
+    // Empties the vector
     _die.clear();
 }
 
 void Dice::add_dice(Die *d)
 {
+    // Adds a die to the _die vector
+    // if it is not nothing
     if (d != NULL)
         _die.push_back(d);
 }
 
 void Dice::roll()
 {
+    // Iterates and rolls all dies
     for (Die *d: _die)
         d->roll();
 }
 
 Die* Dice::die_at(int idx)
 {
+    // Returns a die at an index or returns NULL
     if (idx < _die.size())
         return _die[idx];
     return NULL;
@@ -38,6 +45,7 @@ Die* Dice::die_at(int idx)
 
 int Dice::get_total_value()
 {
+    // Iterates and adds up the face value of all dies
     int value(0);
     for (Die *d: _die)
         value += d->get_top_value();
@@ -46,14 +54,14 @@ int Dice::get_total_value()
 
 bool Dice::all_same()
 {
+    // If there is only one die, all have the same top value
     if (_die.size() == 1) return true;
-    if (_die.size() > 1)
+    // Compare all dies to the first die
+    int value = _die[0]->get_top_value();
+    for (int loopVar(1); loopVar < _die.size(); loopVar++)
     {
-        int value = _die[0]->get_top_value();
-        for (int loopVar(1); loopVar < _die.size(); loopVar++)
-        {
-            if (_die[loopVar]->get_top_value() != value) return false;
-        }
+        // Return false if a different value is encountered
+        if (_die[loopVar]->get_top_value() != value) return false;
     }
     return true;
 }
--- Dice.h
+++ Dice.h
@@ -7,17 +7,28 @@
 #include "Die.h"
 #include <vector>
 
+using namespace std;
+
 class Dice
 {
     private:
+        // Contains all dies
         vector<Die*> _die;
     public:
+        // Constructor
         Dice();
+        // Frees all contained dies
         ~Dice();
+        // Adds a new die
         void add_dice(Die *d);
+        // Rolls all contained dies
         void roll();
+        // Returns the die at a certain index
         Die* die_at(int idx);
+        // Returns the sum of all die top values
         int get_total_value();
+        // Returns the boolean if all dies have the
+        // same face value.
         bool all_same();
 };
 #endif
